2d2a394c9fe7107c7ae9974c0e2b730405d02e95
Luis Roberto Flores <20096938@clases.edu.sv>
2025-04-19 10:38:11 -0600

    Add button component and update StatusBar style in App.js

--- App.js
+++ App.js
@@ -1,15 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
 
 
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <StatusBar style="light" />
       <Image source={{ uri: 'https://www.metacritic.com/a/img/catalog/provider/6/12/6-1-969600-59.jpg' }}
        style={{ width: 215, height: 294 }} />
-      <StatusBar style="auto" />
       <Text style={{ color: 'white' }}>Hola a todos en el mundo</Text>
+      <Button title="soy un boton" onPress={() => alert('presionastes el boton')} /> 
     </View>
   );
 }
