8c4bfd2b7cd26f96fbc67fe2c656a06af8bd3f5e
Luis Roberto Flores <20096938@clases.edu.sv>
2025-04-19 09:55:10 -0600

    Refactor icon import and reorder components in App.js

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
 
-const icon = require('./assets/icon.png');
+import icon from './assets/icon.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hola a todos en el mundo</Text>
       <Image source={icon} style={{ width: 100, height: 100 }} />
       <StatusBar style="auto" />
+      <Text>Hola a todos en el mundo</Text>
     </View>
   );
 }
