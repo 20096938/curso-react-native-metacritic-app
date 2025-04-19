1e100e472a779eb6d0a4527572f2a71c79eae15b
Luis Roberto Flores <20096938@clases.edu.sv>
2025-04-19 10:30:00 -0600

    Add ESLint configuration and implement game data fetching functions

--- App.js
+++ App.js
@@ -6,9 +6,10 @@ import { StyleSheet, Text, View, Image } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Image source={{ uri: 'https://www.metacritic.com/a/img/catalog/provider/6/12/6-1-969600-59.jpg' }} style={{ width: 200, height: 200 }} />
+      <Image source={{ uri: 'https://www.metacritic.com/a/img/catalog/provider/6/12/6-1-969600-59.jpg' }}
+       style={{ width: 215, height: 294 }} />
       <StatusBar style="auto" />
-      <Text>Hola a todos en el mundo</Text>
+      <Text style={{ color: 'white' }}>Hola a todos en el mundo</Text>
     </View>
   );
 }
@@ -16,7 +17,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#09f',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
