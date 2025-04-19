28e6cc3c329dcaa8587b03ca027e5af592109f6c
Luis Roberto Flores <20096938@clases.edu.sv>
2025-04-19 10:10:33 -0600

    Replace local icon import with remote image URL for improved display

--- App.js
+++ App.js
@@ -1,16 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
 
-import icon from './assets/icon.png';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Image source={icon} style={{
-         width: 200,
-          height: 100,
-          resizeMode: 'center',
-           }} />
+      <Image source={{ uri: 'https://www.metacritic.com/a/img/catalog/provider/6/12/6-1-969600-59.jpg' }} style={{ width: 200, height: 200 }} />
       <StatusBar style="auto" />
       <Text>Hola a todos en el mundo</Text>
     </View>
